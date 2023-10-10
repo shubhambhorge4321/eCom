add026f757583a9c1247cfd3ccd0ea7f2cd1a332
shubhambhorge4321 <shubhambhorge4321@gmail.com>
2023-10-10 16:52:11 +0530

    https://github.com/shubhambhorge4321/eCom.git

---
+++ resources/views/master.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <title>E-com project</title>
+</head>
+<body>
+    
+    {{View::make("header")}}
+@yield("form_login")
+
+{{View::make("footer")}}
+
+    
+<script>
+    // $(document).ready(function(){
+    //     $("#btn-1").click(function()
+    //     {
+    //         alert("all right !!");
+    //     })
+    // })
+</script>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+<style>
+    .custom-login{
+        height:400px;
+        padding-top:50px;
+    }
+</style>
+</html>
